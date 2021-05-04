f1106514ce038d3e94b77c95df0065ccb2677275
ham4690 <ham46903@gmail.com>
2021-05-04 23:15:02 +0900

    itp1-3-a

---
+++ java_aoj/ITP1/src/itp1_3/A.java
@@ -0,0 +1,9 @@
+package itp1_3;
+
+public class A {
+    public static void main(String[] args) {
+        for(int i = 0; i < 1000; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
